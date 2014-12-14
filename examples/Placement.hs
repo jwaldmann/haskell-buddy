6dcc8df59fb5ce3c36cf0f75bcd7fe8f9deed277
Johannes Waldmann <waldmann@imn.htwk-leipzig.de>
2014-12-15 00:46:37 +0100

    add examples

---
+++ examples/Placement.hs
@@ -0,0 +1,52 @@
+{-
+Auf wie viele Weisen kann man Spielsteine auf ein 3 x 10-Spielbrett setzen, 
+sodass keine zwei Steine horizontal, vertikal oder diagonal benachbart sind?
+
+(Quelle: Preisaufgabe bei LSGM-Wochenendseminar 2011 in Bennewitz
+http://www.lsgm.de/tiki-index.php?page=Seminare.2011-09)
+
+BUILD:  ghc --make Placement
+RUN  :  ./Placement 3 10
+-}
+
+import Buddy (BDD)
+import qualified Buddy as B
+
+import Control.Monad ( guard, forM_ )
+import System.Environment ( getArgs )
+import qualified Data.Set 
+
+type Position = (Int,Int)
+
+positions :: Int -> Int -> [ Position ]
+positions width height = do 
+    a <- [ 1 .. width  ]
+    b <- [ 1 .. height ]
+    return (a,b)
+
+adjacent :: Position -> Position -> Bool
+adjacent (a,b) (c,d) = 
+    abs (a-c) <= 1 && abs (b-d) <= 1
+  
+main = do
+    args <- getArgs
+    let [ width, height ] = map read args :: [ Int ]
+        ps = positions width height
+    c <- B.run ps $ do
+        bs <- sequence $ do
+           p <- ps
+           q <- ps
+           guard $ p < q 
+           guard $ adjacent p q
+           return $ do
+             x <- B.unit p False
+             y <- B.unit q False
+             x B.|| y
+        b <- B.and bs     
+        B.satcount b   
+    print c
+    B.printstat
+
+
+
+
---
+++ examples/Queens.hs
@@ -0,0 +1,68 @@
+{-
+the N Queens problem. The propositional variables
+correspond to the positions on the board.
+It shows how to construct an OBDD
+and how to check some of its properties.
+
+BUILD:  ghc --make Queens
+RUN  :  ./Queens 7
+-}
+
+import Buddy (BDD)
+import qualified Buddy as B
+
+import Control.Monad ( guard, sequence, forM )
+import Control.Applicative
+
+import System.Environment ( getArgs )
+import qualified Data.Set 
+import qualified Data.Map as M
+
+type Position = (Int,Int)
+
+positions :: Int -> [ Position ]
+positions n = do 
+    a <- [ 1 .. n ]
+    b <- [ 1 .. n ]
+    return (a,b)
+
+threatens :: Position -> Position -> Bool
+threatens (a,b) (c,d) = 
+       a == c     -- same column
+    || b == d     -- same row
+    || a+b == c+d -- same diagonal
+    || a-b == c-d -- same antidiagonal
+
+board n = 
+    B.and =<< sequence ( each_column_is_occupied n ++ no_threats n ) 
+
+for = flip map
+
+each_column_is_occupied n = for [1..n] $ \ a ->
+    B.or =<< forM [ 1 .. n ] ( \ b -> B.unit (a,b) True )
+                            
+each_row_is_occupied n = forM [ 1 .. n ] $ \ a -> 
+    B.or =<< forM [ 1 .. n ] ( \ b ->  B.unit (b,a) True ) 
+
+no_threats n = for (positions n) ( \ p ->
+    B.and =<< forM (positions n) ( \ q ->
+        if p /= q && threatens p q
+        then do a <- B.unit p True ; b <- B.unit q False
+                B.implies a b
+        else B.constant True
+             ) )
+
+main = do
+    [ arg ] <- getArgs
+    mainf $ read arg
+
+mainf n = do 
+    c <- B.run (positions n) $ do
+         b <- board n
+         B.satcount b
+    print c
+    B.printstat
+    
+
+
+
